a49d74300d71ed3aaa7d211a54f01f0042d8450f
KannbalDevelop3 <80915755+KannbalDevelop3@users.noreply.github.com>
2023-11-27 14:38:15 -0600

    ig24Nov23

--- src/config/clienteAxios.jsx
+++ src/config/clienteAxios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 const clienteAxios = axios.create({
-    baseURL: `https://api.kannbal.com/api_crud/api/`
+    baseURL: `https://lms.kannbal.com/api_crud/api/`
 })
 export default clienteAxios
